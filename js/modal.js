02392974b329717a920c6d91c5c1e1c84aad5987
Sergio Silva <sergjsilva@gmail.com>
2024-08-01 00:00:03 +0200

    updating files

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const contactForm = document.querySelector("#contact-form");
 
 contactForm.addEventListener("submit", (event) => {
@@ -18,3 +19,25 @@ contactForm.addEventListener("submit", (event) => {
 
   window.open(formURL, "_blank");
 });
+=======
+const contactForm = document.querySelector("#contact-form");
+
+contactForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const name = contactForm.querySelector("#name").value;
+  const email = contactForm.querySelector("#email").value;
+  const subject = contactForm.querySelector("#subject").value;
+  const message = contactForm.querySelector("#message").value;
+
+  const formURL = `https://docs.google.com/forms/d/e/1FAIpQLSdd8mRzsAanrx9TbpnLlU-x3H9V34ti5TkdDTi0qNbHv_iUfA/viewform?usp=pp_url&entry.1881277022=${name}&entry.2125381079=${email}&entry.1468752291=${subject}&entry.77150289=${message}`;
+
+  // Close the modal
+  const modal = bootstrap.Modal.getInstance(
+    document.querySelector("#contactModal")
+  );
+  modal.hide();
+
+  window.open(formURL, "_blank");
+});
+>>>>>>> d996ea5 (updating code)
--- js/nav.js
+++ js/nav.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let path = window.location.pathname.split("/").pop();
 path = `./${path}`;
 
@@ -12,3 +13,19 @@ navLinks.forEach((link) => {
     link.classList.remove("active");
   }
 });
+=======
+let path = window.location.pathname.split("/").pop();
+path = `./${path}`;
+
+const navbar = document.querySelector(".navbar-nav");
+let navLinks = navbar.querySelectorAll("a");
+
+navLinks.forEach((link) => {
+  const href = link.getAttribute("href");
+  if (href === path) {
+    link.classList.add("active");
+  } else {
+    link.classList.remove("active");
+  }
+});
+>>>>>>> d996ea5 (updating code)
