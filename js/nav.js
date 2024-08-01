48d202d1bca8d77ecb4679794d34e412eaaf25b1
Sergio Silva <sergjsilva@gmail.com>
2024-08-01 07:31:17 +0200

    Add files via upload

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,11 @@
-let path = window.location.pathname.split("/").pop();
-
-if (path === "" || path === "/") {
+let path = window.location.pathname;
+console.log(path);
+// Handle the root URL case and pretty URLs
+if (path === "/" || path === "/index.html") {
   path = "./index.html";
 } else {
-  path = `./${path}`;
+  // Prepend "./" for consistency with href attributes
+  path = `.${path}`;
 }
 
 const navbar = document.querySelector(".navbar-nav");
