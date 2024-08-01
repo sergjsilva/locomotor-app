100ae547b919a6c7db133e267e38a6466c4c8388
Sergio Silva <sergjsilva@gmail.com>
2024-08-01 08:31:17 +0200

    Add files via upload

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,4 @@
 let path = window.location.pathname;
-console.log(path);
-
-console.log(path);
 const navbar = document.querySelector(".navbar-nav");
 let navLinks = navbar.querySelectorAll("a");
 
@@ -11,9 +8,7 @@ navLinks.forEach((link) => {
   if (href === "/index.html") href = "/";
   if (href === path) {
     link.classList.add("active");
-    console.log(link);
   } else {
-    console.log(`HREF:${href} .... PATH:${path}`);
     link.classList.remove("active");
   }
 });
